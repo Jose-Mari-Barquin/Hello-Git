ca0cb56c6182eff0f26bbd378f0fa80f4fb35208
JoseM <josema_b@outlook.com>
2024-08-16 14:22:26 +0200

    Git 3 v2

--- Hellogit2.cs
+++ Hellogit2.cs
@@ -1,5 +1,9 @@
 internal class Program
 {
+    /// <summary>
+    /// print hello git3 v2
+    /// </summary>
+    /// <param name="args"></param>
     private static void Main(string[] args)
     {
         public void main()
