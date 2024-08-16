0588c18cd56153136e59a3149000824e1a81de27
JoseM <josema_b@outlook.com>
2024-08-16 13:51:59 +0200

    Login

--- Hellogit2.cs
+++ Hellogit2.cs
@@ -1,5 +1,9 @@
 internal class Program
 {
+    /// <summary>
+    /// RAMA LOGIN MODIFICACION
+    /// </summary>
+    /// <param name="args"></param>
     private static void Main(string[] args)
     {
         public void main()
